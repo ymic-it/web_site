6a539d2d5fe1aa9debb6b708a9912073cb3b173e
root <shogo070920@gmail.com>
2016-01-07 00:04:59 +0900

    [add]indexpage

---
+++ application/views/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--Language = Ja-->
+<html lang="ja">
+<!--charset is utf-8-->
+<meta charset="utf-8">
+
+<!--ビューポート設定-->
+<mate name="viewport" content="width=device-width,initial-scal=1"></mate>
+
+
+<!--Loding CSSdate-->
+<link href="<?= base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+<link href="<?= base_url(); ?>css/ymic.css" rel="stylesheet">
+<link href="<?= base_url(); ?>js/bootstrap.js">
+<link href="<?= base_url(); ?>js/bootstrap.min.js">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<link rel="shortcut icon" href="favicon1.ico" type="image/vnd.microsoft.icon">
+
+</html>
+
+<div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+
+        <div class="navbar-header" background-color="#2FA4E7">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="index">横浜医療情報専門学校プログラミングサークル
+                <br>
+                <small>Yokohma Medical Information Programming Group</small>
+            </a>
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+        </div>
+
+    </div>
+</div>
+
+</br>
+</br>
+</br>
+
+
