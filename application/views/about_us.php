a97403c16f4621c19ac1ed6cd1d9495473c69a62
root <shogo070920@gmail.com>
2016-01-10 22:06:34 +0900

    [add]aboutas css

---
+++ application/views/about_us.php
@@ -0,0 +1,42 @@
+<link href="<?= base_url(); ?>css/about_us.css" rel="stylesheet">
+<div class="ct" id="t1">
+    <div class="ct" id="t2">
+        <div class="ct" id="t3">
+            <div class="ct" id="t4">
+                <section>
+                    <ul>
+                        <a href="#t1">
+                            <li class="icon fa fa-home" id="uno">Abstract</li>
+                        </a>
+                        <a href="#t2">
+                            <li class="icon fa fa-keyboard-o" id="dos">Member</li>
+                        </a>
+                        <a href="#t3">
+                            <li class="icon fa fa-coffee" id="tres">Join us!</li>
+                        </a>
+                        <a href="index">
+                            <li class="icon fa fa-coffee" id="tres">back</li>
+                        </a>
+                    </ul>
+                    <div class="page" id="p1">
+                        <li class="icon fa fa-home">
+                            <span class="title">どんなサークルなのん？</span>
+                            <span class="hint"> 2015年発足したばかりの弱小サークル。
+      現在総員5人という小規模で活動中。<br>
+      現在このホームページを運営しつつアプリ開発とか、ネットワークとか設計とかとかやってます。<br>
+      決してワンマン運営ではございませんよ！？<br>
+      これからの活躍に乞うご期待！<br></span></li>
+                        <p class="credit"> Original Pen by <a href="http://codepen.io/hrtzt/">Alberto Hartzet</a></p>
+                    </div>
+                    <div class="page" id="p2">
+                        <li class="icon fa fa-keyboard-o"><span class="title">メンバー</span></li>
+                    </div>
+                    <div class="page" id="p3">
+                        <li class="icon fa fa-coffee"><span class="title">入部希望の方へ</span></li>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </div>
+</div>
